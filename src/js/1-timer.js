f157e0e041107214a9029281058df91bad7a2e8d
Oleg007git <cusniroleg@icloud.com>
2024-03-19 18:24:42 -0700

    1

---
+++ src/js/1-timer.js
@@ -0,0 +1,57 @@
+import flatpickr from 'flatpickr';
+
+import 'flatpickr/dist/flatpickr.min.css';
+
+import iziToast from 'izitoast';
+
+import 'izitoast/dist/css/iziToast.min.css';
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+};
+
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = Math.floor(ms / day);
+
+  const hours = Math.floor((ms % day) / hour);
+
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
+console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
+console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
+
+const timer = document.querySelector('.timer');
+const field = document.querySelector('field');
+const value = document.querySelector('.value');
+const label = document.querySelector('label');
+const text = document.querySelector('#datetime-picker');
+const startBtn = document.querySelector('[data-start]');
+
+startBtn.addEventListener('click', () => {
+  const initTime = Date.now();
+
+  setInterval(() => {
+    const currentTime = Date.now();
+    const diff = currentTime - initTime;
+    const time = convertMs(diff);
+    console.log(time);
+    text.innerHTML = time;
+  }, 1000);
+});
